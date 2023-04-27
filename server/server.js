9fb456bf78ca0bddaa5a9062d0e3d4d6549e9244
Breno Brasil <breno2019222@gmail.com>
2023-04-27 00:06:48 -0300

    Update

--- server/server.js
+++ server/server.js
@@ -36,15 +36,19 @@ app.use('/api/user_session', UserRoutes)
 
 const __dirname = path.resolve()
 
-/* if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')))
+
+    app.get('/', (req, res) => {
+        console.log('API is running...')
+    })
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => {
         res.send('API is running...')
     })
-} */
+}
 
 const PORT = process.env.PORT || 5000;
 
