96fb3b79d2fa8e4d1ad2169052e54e7df6c04377
Breno Brasil <breno2019222@gmail.com>
2023-04-26 21:03:17 -0300

    Update

--- server/server.js
+++ server/server.js
@@ -39,10 +39,6 @@ const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')))
 
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
-
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => {
