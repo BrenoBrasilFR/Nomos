a180eb02c73d2aec7f734b1d4cacfbf5639bb814
Breno Brasil <breno2019222@gmail.com>
2023-05-17 14:53:23 -0300

    Update

--- client/src/pages/home/CarouselItems.js
+++ client/src/pages/home/CarouselItems.js
@@ -2,19 +2,12 @@ import { Link } from "react-router-dom";
 
 const CarouselItems = ({ product }) => {
 
-    if (product.product_name === 'Ludwig' || product.product_name === 'Lambda') {
-        return <div className="carousel-item">
-            <Link to={`/shop/product/${product.id}`} state={{ from: "home" }}>
-                <img src={`images/collection/${product.product_name}.jpg`} alt={product.product_name}></img><h4>{product.product_name}</h4>
-            </Link>
-        </div>
-    } else {
-        return <div className="carousel-item">
-            <Link to={`/shop/product/${product.id}`} state={{ from: "home" }}>
-                <img src={`images/collection/${product.product_name}.PNG`} alt={product.product_name} /><h4>{product.product_name}</h4>
-            </Link>
-        </div>
-    }
+    return <div className="carousel-item">
+        <Link to={`/shop/product/${product.id}`} state={{ from: "home" }}>
+            <img src={product.image_url} alt={product.product_name}></img><h4>{product.product_name}</h4>
+        </Link>
+    </div>
+
 }
 
 export default CarouselItems
